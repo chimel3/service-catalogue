fc3fad7f52d46d9c8495005b5aada3dd30a331c0
Tim Mitchell <tim_mitchell@outlook.com>
2019-05-09 22:39:52 +0100

    Working code for real cosmos DB with automated test file and requirements.txt

---
+++ config_cosmos_azure.py
@@ -0,0 +1,13 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'WJzqptnDIw5behk2LdSs99qtzIGGLoxD956TAtNjXFgzw2eZZRuYYB3xGAxrvneXKYc3l4aOqhbawAkBEVte7w=='
+
+COSMOSDB_HOST = 'https://tm-cosmos-001.documents.azure.com:443/'
+COSMOSDB_KEY = 'WJzqptnDIw5behk2LdSs99qtzIGGLoxD956TAtNjXFgzw2eZZRuYYB3xGAxrvneXKYc3l4aOqhbawAkBEVte7w=='
+
+COSMOSDB_DATABASE = 'service catalogue'
+COSMOSDB_COLLECTION_VM = 'virtualmachines'
+COSMOSDB_COLLECTION_VMID = 'vmid'
+COSMOSDB_COLLECTION_IP = 'ipaddresses'
+COSMOSDB_COLLECTION_FW = 'firewallrules'
+COSMOSDB_COLLECTION_FWID = 'fwid'
+COSMOSDB_COLLECTION_SUBNET = 'subnets'
